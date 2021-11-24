a53698043805f66a2cfe2f5f8a7ef8c9e9f316c9
“janeqqrul” <jan.karolewski1b@gmail.com>
2021-11-24 08:34:01 +0100

    Add GRAPHENE SCHEMA
    eneble main schema for GraphQL

--- Movie_GraphQL/Movie_GraphQL/settings.py
+++ Movie_GraphQL/Movie_GraphQL/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from graphene.types import schema
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'movies'
+    'Movie_GraphQL.movies',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# GRAPHENE SCHEMAS
+
+
+GRAPHENE = {
+    "SCHEMA" : "Movie_GraphQL.schema.schema"
+}
+
+
