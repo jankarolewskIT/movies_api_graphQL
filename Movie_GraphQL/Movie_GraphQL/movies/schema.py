aed977fa78f5eba8d17ed76c8e8d89b5d5283943
“janeqqrul” <jan.karolewski1b@gmail.com>
2021-11-24 10:21:19 +0100

    Add logic to schema

--- Movie_GraphQL/Movie_GraphQL/movies/schema.py
+++ Movie_GraphQL/Movie_GraphQL/movies/schema.py
@@ -1,14 +1,39 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 
-from .models import Movie
+from .models import Movie, Director
 
 class MovieType(DjangoObjectType):
     class Meta:
         model = Movie
 
+    movie_age = graphene.String()
+
+    def resolve_movie_age(self, info):
+        return "Old movie" if self.year < 2000 else "New movie"
+
+class DirectorType(DjangoObjectType):
+    class Meta:
+        model = Director
+
 class Query(graphene.ObjectType):
     all_movies = graphene.List(MovieType)
+    movie = graphene.Field(MovieType, id=graphene.Int(), title=graphene.String())
+    all_directors = graphene.List(DirectorType)
 
     def resolve_all_movies(self, info, **kwargs):
         return Movie.objects.all()
+
+    def resolve_movie(self, info, **kwargs):
+        id = kwargs.get('id')
+        title = kwargs.get('title')
+        if id:
+            return Movie.objects.get(pk=id)
+
+        if title:
+            return Movie.objects.get(title=title) 
+
+        return None
+    
+    def resolve_all_directors(self, info, **kwargs):
+        return Director.objects.all()
